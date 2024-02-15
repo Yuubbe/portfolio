e02536139d55093fed63888fd3ac281b7be6be8c
Yuubbe <120384600+Yuubbe@users.noreply.github.com>
2024-02-15 16:40:13 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GenshinMenu
+{
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer backgroundMusicPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            backgroundMusicPlayer = new MediaPlayer();
+            backgroundMusicPlayer.Open(new Uri("C:/Users/gambvin/Downloads/L’Étoile D’Afrique x Les Trompettes D’Afrique.mp3"));
+            backgroundMusicPlayer.Volume = 0.5; // Définir le volume initial à 50%
+        }
+
+        
+    }
+}
